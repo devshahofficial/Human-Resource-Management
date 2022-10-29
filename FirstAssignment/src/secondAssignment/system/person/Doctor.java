a768501e9db9bfa3cd795288cf4a38d8c7843bc1
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2022-10-29 18:10:10 -0400

    Basic additions

---
+++ FirstAssignment/src/secondAssignment/system/person/Doctor.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package secondAssignment.system.person;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ *
+ * @author devshahofficial
+ */
+public class Doctor extends Person {
+    
+    public Doctor(String name, Integer patientID, Integer age, String email, City city, House house, Community community) {
+        super(name, patientID, age, email, city, house, community);
+    }
+    
+    
+    
+}
